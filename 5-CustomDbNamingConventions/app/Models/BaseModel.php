3abf49ea131d73ace6126d808d0a13e507d84b26
Damian Suess <djsuess@gmail.com>
2024-05-06 10:54:20 -0400

    2 spaces

--- 5-CustomDbNamingConventions/app/Models/BaseModel.php
+++ 5-CustomDbNamingConventions/app/Models/BaseModel.php
@@ -7,17 +7,17 @@ use Illuminate\Support\Str;
 
 class BaseModel extends Model
 {
-    const CREATED_AT = "CreatedAt";
-    const UPDATED_AT = "UpdatedAt";
-    const DELETED_AT = "DeletedAt";
+  const CREATED_AT = "CreatedAt";
+  const UPDATED_AT = "UpdatedAt";
+  const DELETED_AT = "DeletedAt";
 
-    public function getTable()
-    {
-        return $this->table ?? Str::singular(class_basename($this));
+  public function getTable()
+  {
+    return $this->table ?? Str::singular(class_basename($this));
 
-        // Pluralize
-        // return $this->table ?? Str::pluralStudly(class_basename($this));
-        // Snake
-        // return $this->table ?? Str::snake(Str::pluralStudly(class_basename($this)));
-    }
+    // Pluralize
+    // return $this->table ?? Str::pluralStudly(class_basename($this));
+    // Snake
+    // return $this->table ?? Str::snake(Str::pluralStudly(class_basename($this)));
+  }
 }
