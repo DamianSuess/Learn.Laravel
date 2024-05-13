2cccd024e7fe650ca1a6be007f83bf164d972ca1
Damian Suess <djsuess@gmail.com>
2024-05-13 19:34:18 -0400

    PascalCase REST Api with Filtering

---
+++ 5.3-PascalCase-REST-Api-Filtering/app/Models/BaseModel.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class BaseModel extends Model
+{
+
+  /**
+   * The name of the "created at" column.
+   *
+   * @var string|null
+   */
+  const CREATED_AT = "CreatedAt";
+
+
+  /**
+   * The name of the "updated at" column.
+   *
+   * @var string|null
+   */
+  const UPDATED_AT = "UpdatedAt";
+
+  /**
+   * The name of the "deleted at" column.
+   *
+   * @var string|null
+   */
+  const DELETED_AT = "DeletedAt";
+
+  public function getTable()
+  {
+    return $this->table ?? Str::singular(class_basename($this));
+
+    // Pluralize
+    // return $this->table ?? Str::pluralStudly(class_basename($this));
+    // Snake
+    // return $this->table ?? Str::snake(Str::pluralStudly(class_basename($this)));
+  }
+}
