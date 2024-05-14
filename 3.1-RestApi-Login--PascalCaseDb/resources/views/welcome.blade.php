91913609a82f4be032217b6b4966fdd55a3676fe
Damian Suess <djsuess@gmail.com>
2024-05-14 09:11:49 -0400

    Copy of 3.0-RestApi-Login project

---
+++ 3.1-RestApi-Login--PascalCaseDb/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+
+  <body>
+    <ul>
+      <li><a href="api/register">POST - api/register</a></li>
+      <li><a href="api/login">POST - api/login</a></li>
+    </ul>
+  </body>
+
+</html>
