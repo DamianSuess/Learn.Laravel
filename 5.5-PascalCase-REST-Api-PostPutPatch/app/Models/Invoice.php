adb53f4b03bbc89508fca07117a3497484ec3a79
Damian Suess <djsuess@gmail.com>
2024-05-16 22:03:31 -0400

    cleanup

--- 5.5-PascalCase-REST-Api-PostPutPatch/app/Models/Invoice.php
+++ 5.5-PascalCase-REST-Api-PostPutPatch/app/Models/Invoice.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
+use App\Models\BaseModel;
 use App\Models\Customer;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
 class Invoice extends BaseModel
