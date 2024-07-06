68d6530ffc3f71605f78273f61251cf792ecc7a8
Damian Suess <djsuess@gmail.com>
2024-07-06 18:49:19 -0400

    php-cs-fixer on whole project

--- 7.0-Database-PivotTable/database/migrations/2024_07_06_212021_create_customer.php
+++ 7.0-Database-PivotTable/database/migrations/2024_07_06_212021_create_customer.php
@@ -4,31 +4,30 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
   /**
    * Run the migrations.
    */
   public function up(): void
   {
-      Schema::create('customer', function (Blueprint $table) {
+    Schema::create('customer', function (Blueprint $table) {
       $table->id();
       $table->string("name");
       $table->timestamps();
     });
 
-    Schema::create("flights", function(Blueprint $table) {
+    Schema::create("flights", function (Blueprint $table) {
       $table->id();
       $table->string("name");
       $table->string("airline");
       $table->timestamps();
     });
 
-    Schema::create("customer_flight", function(Blueprint $table) {
+    Schema::create("customer_flight", function (Blueprint $table) {
       $table->id();
       $table->string("customer_id");
       $table->string("flight_id");
-      $table->primary(["customer_id", "flight_id"]  );
+      $table->primary(["customer_id", "flight_id"]);
     });
   }
 
