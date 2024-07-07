f76be94be3af6f3e23409caeaa6b89cc7462f830
Damian Suess <djsuess@gmail.com>
2024-07-07 12:17:34 -0400

    refactor

--- 7.0-Database-PivotTable/app/Models/Invoice.php
+++ 7.0-Database-PivotTable/app/Models/Invoice.php
@@ -23,7 +23,7 @@ class Invoice extends Model
       )
       ->as("invoiceitem")     // Custom pivot name for the relationship (default: `pivot`)
       ->withTimestamps()      // Update the `created_at` and `updated_at` timestamps
-      ->withPivot(["seat"])   // Columns on the pivot table to retrieve
+      ->withPivot(["coupon"])   // Columns on the pivot table to retrieve
       ->using(InvoiceItem::class); // Custom pivot model used in the relationship
   }
 }
--- 7.0-Database-PivotTable/app/Models/Product.php
+++ 7.0-Database-PivotTable/app/Models/Product.php
@@ -20,7 +20,7 @@ class Product extends Model
       )
       ->as("invoiceitem")           // Custom pivot name for the relationship (default: `pivot`)
       ->withTimestamps()            // Update the `created_at` and `updated_at` timestamps
-      ->withPivot(["seat"])         // Columns on the pivot table to retrieve
+      ->withPivot(["coupon"])         // Columns on the pivot table to retrieve
       ->using(InvoiceItem::class);  // Custom pivot model used in the relationship
   }
 }
