690d0b1899c354201477f4e55f98b3ccd192b6d9
Damian Suess <djsuess@gmail.com>
2025-05-11 08:41:00 -0400

    cleanup

--- 11.0-PermissionRules/config/session.php
+++ 11.0-PermissionRules/config/session.php
@@ -128,8 +128,8 @@ return [
     */
 
     'cookie' => env(
-        'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+      'SESSION_COOKIE',
+      Str::slug(env('APP_NAME', 'laravel'), '_') . '_session'
     ),
 
     /*
